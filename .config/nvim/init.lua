445ca0e5f4e5d9239471c3e15fadaf7a1d45724d
eric <69027452+gbslr@users.noreply.github.com>
2025-01-26 08:49:47 -0500

    treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
-vim.o.termguicolors = false
-vim.o.number = true
-vim.cmd.colorscheme('wpgtk')
+vim.o.termguicolors = false -- no clue why but this being on breaks colors even though it should do the opposite? 
+vim.o.ignorecase = true
+vim.o.number = true -- line numbers
+vim.cmd.colorscheme('wpgtk') -- nicer than base terminal colorscheme
 
 require("config.lazy")
+
